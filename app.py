0de7d57db9fcba8a8803feba5e3140efd02d74fe
Bruno-Pegado <Brunopegado128@gmail.com>
2024-10-23 18:06:34 +0530

    accuracy

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-import time
 from src.utils.preprocess import preprocess
 from src.tf_idf.td_idf import tf_idf
 from src.utils.dictionaries import synonym_dict
+from rouge import Rouge
 
 app = Flask(__name__)
 
@@ -19,8 +19,7 @@ def summarize():
     if not paragraph.strip():
         original_text = "No text provided"
     else:
-        original_text = paragraph  # Store original text for display
-        start_time = time.time()
+        original_text = paragraph
 
         op = preprocess(paragraph)
         sentences = [' '.join(sentence) for sentence in op]
@@ -35,7 +34,7 @@ def summarize():
         sentence_scores.sort(key=lambda x: x[1], reverse=True)
         
         n = len(sentence_scores)
-        a = round(n/2)
+        a = round(n * 0.7)
         top_sentences_idx = [idx for idx, score in sentence_scores[:a]]
         
         top_sentences_idx.sort()
@@ -43,12 +42,12 @@ def summarize():
         summarized_text = " ".join([sentences[idx] for idx in top_sentences_idx])
 
         summarized_text = replace_synonyms(summarized_text)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        
-        execution_time = f"The execution time is: {elapsed_time:.4f} seconds"
 
-    return render_template('index.html', original_text=original_text, summary_text=summarized_text)
+        accuracy = calculate_accuracy(original_text, summarized_text)
+        for rouge in accuracy.items():
+            print(rouge)
+
+    return render_template('index.html', original_text=original_text, summary_text=summarized_text, accuracy=accuracy)
 
 def replace_synonyms(text: str) -> str:
     words = text.split()
@@ -56,6 +55,31 @@ def replace_synonyms(text: str) -> str:
     replaced_words = [synonym_dict.get(word, word) for word in words]
 
     return ' '.join(replaced_words)
+
+def calculate_accuracy(original_text: str, summary_text: str) -> dict:
+    rouge = Rouge()
+    scores = rouge.get_scores(summary_text, original_text)
+    rouge_1 = scores[0]['rouge-1']
+    rouge_2 = scores[0]['rouge-2']
+    rouge_l = scores[0]['rouge-l']
+    return {
+        "ROUGE-1": {
+            "Precision": round(rouge_1['p'] * 100, 2),
+            "Recall": round(rouge_1['r'] * 100, 2),
+            "F1-Score": round(rouge_1['f'] * 100, 2),
+        },
+        "ROUGE-2": {
+            "Precision": round(rouge_2['p'] * 100, 2),
+            "Recall": round(rouge_2['r'] * 100, 2),
+            "F1-Score": round(rouge_2['f'] * 100, 2),
+        },
+        "ROUGE-L": {
+            "Precision": round(rouge_l['p'] * 100, 2),
+            "Recall": round(rouge_l['r'] * 100, 2),
+            "F1-Score": round(rouge_l['f'] * 100, 2),
+        }
+    }
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/tf_idf/td_idf.py
+++ src/tf_idf/td_idf.py
@@ -1,7 +1,6 @@
 import math
 from collections import defaultdict
 
-# Step 1: Term Frequency (TF)
 def compute_tf(word_list):
     tf_dict = {}
     total_words = len(word_list)
@@ -9,30 +8,25 @@ def compute_tf(word_list):
     for word in word_list:
         tf_dict[word] = tf_dict.get(word, 0) + 1
     
-    # Normalize by the number of words in the sentence
     for word in tf_dict:
         tf_dict[word] /= total_words
     
     return tf_dict
 
-# Step 2: Inverse Document Frequency (IDF)
 def compute_idf(processed_text):
     num_sentences = len(processed_text)
     idf_dict = defaultdict(int)
     
-    # Count the number of sentences containing each word
     for sentence in processed_text:
-        for word in set(sentence):  # Using set to count only once per sentence
+        for word in set(sentence):
             if word:
                 idf_dict[word] += 1
     
-    # Calculate the IDF for each word
     for word in idf_dict:
         idf_dict[word] = math.log(num_sentences / float(idf_dict[word]))
     
     return idf_dict
 
-# Step 3: TF-IDF Calculation
 def compute_tf_idf(tf_dict, idf_dict):
     tf_idf_dict = {}
     
@@ -41,7 +35,6 @@ def compute_tf_idf(tf_dict, idf_dict):
     
     return tf_idf_dict
 
-# Step 4: Putting it all together
 def tf_idf(processed_text):
     idf_dict = compute_idf(processed_text)
     
